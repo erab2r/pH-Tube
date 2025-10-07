3c350a9e624b3d9486cbcd529673101b9c442ae0
erab2r <shkera213@gmail.com>
2025-10-08 00:30:43 +0600

    1

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,19 @@ console.log("ccc");
 function loadCategories() {
     fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
     .then((res)=>res.json())
-    .then((data)=>console.log(data.categories));
+    .then((data)=>displayCategories(data.categories));
 }
+
+function displayCategories(categories) {
+  const categoryContainer = document.getElementById("category-container"); 
+
+  for (let cat of categories){
+    const categoryDiv = document.createElement("div");
+    categoryDiv.innerHTML = `
+      <button class="btn btn-sm">${cat.category}</button>
+    `;
+    categoryContainer.appendChild(categoryDiv);
+  }     
+}
+
 loadCategories();
