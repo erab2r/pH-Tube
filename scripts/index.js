244a5737b3061dd7f6362e9c2ee10368d92efd99
erab2r <shkera213@gmail.com>
2025-10-07 23:55:04 +0600

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+console.log("ccc");
+
+function loadCategories() {
+    fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((res)=>res.json())
+    .then((data)=>console.log(data.categories));
+}
+loadCategories();
