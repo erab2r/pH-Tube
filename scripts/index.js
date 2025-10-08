92989571a734b28fbfdf8f05bc7fd85ee541a75c
erab2r <shkera213@gmail.com>
2025-10-08 10:31:52 +0600

    1

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,11 @@ function loadCategories() {
     .then((res)=>res.json())
     .then((data)=>displayCategories(data.categories));
 }
+function loadVideos(){
+   fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+   .then(response=>response.json())
+   .then((data)=>displayVideos(data.videos));
+}
 
 function displayCategories(categories) {
   const categoryContainer = document.getElementById("category-container"); 
@@ -12,10 +17,48 @@ function displayCategories(categories) {
   for (let cat of categories){
     const categoryDiv = document.createElement("div");
     categoryDiv.innerHTML = `
-      <button class="btn btn-sm">${cat.category}</button>
+      <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
     `;
     categoryContainer.appendChild(categoryDiv);
   }     
 }
+const displayVideos = (videos) =>{
+    const videoContainer = document.getElementById("video-container");
+    
+    videos.forEach(video=>{
+        // console.log(videos);
+        const videoCard = document.createElement("div");
+        videoCard.innerHTML = `
+<div class="card bg-base-100">
+  <figure class="relative">
+    <img class = "w-full h-[150px] object-cover"
+      src="${video.thumbnail}"
+      alt="Shoes" />
+      <span class="absolute bottom-2 right-2 text-white text-sm bg-black px-2 rounded">3hrs 56 min ago</span>
+  </figure>
+  <div class="flex gap-3 px-0 py-5">
+        <div class="profile">
+            <div class="avatar">
+                <div class="ring-primary ring-offset-base-100 w-6 rounded-full ring-2 ring-offset-2">
+                    <img src="${video.authors[0].profile_picture}" />
+                </div>
+            </div>
+        </div>
+        <div class="intro">
+            <h2 class="text-sm font-semibold">Midnight Serenade</h2>
+            <p class="text-sm text-gray-400 flex gap-1">Awlad Hossain 
+                <img
+                class="w-5 h-5" 
+                src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png" alt="">
+            </p>
+            <p class="text-sm text-gray-400">91K views</p>
+        </div>
+  </div>
+</div>
+        `
+        videoContainer.appendChild(videoCard);
+    })
+}
 
 loadCategories();
+loadVideos();
